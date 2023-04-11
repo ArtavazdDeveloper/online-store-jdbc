d195d3c966e44b5a4f2a725fe2a09564642470b2
ArtavazdDeveloper <artavazd1993@gmail.com>
2023-04-12 02:42:17 +0400

    My repository

---
+++ src/main/java/eshop/model/Category.java
@@ -0,0 +1,15 @@
+package eshop.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class Category {
+
+    private int id;
+    private String name;
+}
